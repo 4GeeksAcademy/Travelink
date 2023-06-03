4a4b339e2d7cef78c27f5ad472675e0d5342965a
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-06-03 01:45:44 +0000

    Componente CardHome agregado

---
+++ src/front/js/component/cardHome.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export const CardHome = () => (
+    <>
+        {/* <div className="col-2 m-2 border around-5">
+            <img className="card-img-top" src="https://haimaneltroudi.com/wp-content/uploads/2020/07/haiman-el-troudi-roraima-el-gran-verde-azulado-de-la-casa-de-los-dioses.jpg" alt="Card image cap"/>
+            <div className="card-body">
+                <h4>Tour Roraima - Venezuela</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5>$200 por noche</h5>
+            </div>
+        </div> */}
+        <div className="row row-col-1 row-col-md-4 g-4 m-5">
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        </div>
+                    </div>
+         </div>
+    </>
+)
+	
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Filterbar } from "../component/filterbar.js";
+import { CardHome } from "../component/cardHome.js";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -10,6 +11,8 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<Filterbar/>
+			<CardHome/>
+			<CardHome/>
 		</div>
 	);
 };
