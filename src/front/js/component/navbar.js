08b153f0b2a734dc07b103dd10e26a8300ec3fcf
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2023-06-06 11:34:04 +0000

    Logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,19 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import travelinkLogo from "../../img/Travelink.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light ">
 			<div className="container">
 				<Link to="/" className="link-navbar-brand">
-					<span className="navbar-brand mb-0 fs-4 ">TraveLink</span>
+					<img src={travelinkLogo} className="logoNavbar" />
+					{/* <span className="navbar-brand mb-0 fs-4 ">TraveLink</span> */}
 				</Link>
 				<div className="">
 					<Link to="/login">
 						<button className="btn btn-travelink rounded-pill">Log in</button>
 					</Link>
-					<FontAwesomeIcon className="ms-4" icon={faMagnifyingGlass} style={{color: "#4dd7fa",}} />
+					<FontAwesomeIcon className="ms-4" icon={faMagnifyingGlass} style={{ color: "#4dd7fa", }} />
 				</div>
 			</div>
 		</nav>
