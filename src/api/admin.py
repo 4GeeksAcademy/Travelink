120d3fa7cddb979047fe05166c35d621cc468ee5
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-06-08 00:02:42 +0000

    Vistas de tablas agregadas

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Agencia, Viajero, Viaje, Estatus_Viaje, Viaje_Reserva, Estatus_Reserva, Agencia_Favorito
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,13 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-
+    admin.add_view(ModelView(Agencia, db.session))
+    admin.add_view(ModelView(Viajero, db.session))
+    admin.add_view(ModelView(Viaje, db.session))
+    admin.add_view(ModelView(Estatus_Viaje, db.session))
+    admin.add_view(ModelView(Viaje_Reserva, db.session))
+    admin.add_view(ModelView(Estatus_Reserva, db.session))
+    admin.add_view(ModelView(Agencia_Favorito, db.session))
+    
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
