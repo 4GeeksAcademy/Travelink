7105bde51ab18b5969f64901602b07df087f2d77
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-06-29 18:47:45 +0000

    diseño card paquetes de agencias mejorado

--- src/front/js/component/cardPackageAgency.js
+++ src/front/js/component/cardPackageAgency.js
@@ -51,45 +51,32 @@ export const CardPackageAgency = (props) => {
     };
 
     return (
-        <div className="card col-lg-5 col-md-12 col-sm-12 col-xs-12 my-2 mx-2 p-0" >
-            <div className="row g-0 h-100">
-                <div className="col-md-4">
-                    <img src={props.paquete.img_paquete} className="img-fluid rounded-start" style={{ height: "100%", width: "100%" }} alt="..." />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">{props.paquete.title}</h5>
-                        <p className="card-text">{trickText(props.paquete.description)}</p>
-                        <div className="container justify-content-end">
-                            <Link to={"/edit-Package/" + props.paquete.id}>
-                                <button className="m-1 btn btn-travelink btn btn-outline-info rounded-pill">Editar</button>
-                            </Link>
-                            <button onClick={() => DeletePackage()} className="m-1 btn btn-outline-info rounded-pill">Eliminar</button>
-
-                            {/* <button type="button" className="m-1 btn btn-outline-info rounded-pill" data-toggle="modal" data-target="#exampleModal">
-                                Eliminar
-                            </button>
-
-                            {/* <!-- Modal --> */}
-                            {/* <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div className="modal-dialog" role="document">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div className="modal-body">
-                                            ...
-                                        </div>
-                                        <div className="modal-footer">
-                                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            <button type="button" className="btn btn-primary">Save changes</button>
-                                        </div>
-                                    </div>
+        <div className="m-2">
+            <div className="card m-4 mb-3 col-12 p-0" >
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={props.paquete.img_paquete} className="img-fluid rounded-start h-100 w-100" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <div className="d-flex justify-content-between">
+                                <h5 className="card-title">{props.paquete.title}</h5>
+                                <h5 className="card-title">Destino: {props.paquete.destination}</h5>
+                            </div>
+                            <hr></hr>
+                            <div className="row">
+                                <div className="col-8">
+                                    <p className="card-text">{trickText(props.paquete.description)}</p>
+                                    <p>Costo de Reserva: {props.paquete.reservation_cost}</p>
+                                    <p className="card-text">Costo Total: {props.paquete.total_cost}</p>
                                 </div>
-                            </div> */}
+                                <div className="col-4">
+                                    <Link to={"/edit-Package/" + props.paquete.id}>
+                                        <button className="col-md-12 col-sm-12 col-xs-12 mx-1 btn btn-travelink btn btn-info rounded-pill">Editar</button>
+                                    </Link>
+                                    <button onClick={() => DeletePackage()} className="col-md-12 col-sm-12 col-xs-12 mx-1 btn btn-outline-secondary rounded-pill">Eliminar</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
